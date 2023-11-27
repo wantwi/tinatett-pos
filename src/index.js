c84fc29a925119f619ac80e5159bb91bdea4f230
wantwi <wantwi28@gmail.com>
2023-11-27 07:58:34 +0000

    adding syncfussion package

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Main from './EntryFile/Main';
+import { registerLicense } from '@syncfusion/ej2-base';
+
+// Registering Syncfusion license key
+registerLicense('ORg4AjUWIQA/Gnt2VlhiQlVPd11dXmJWd1p/THNYflR1fV9DaUwxOX1dQl9gSH9RcEVrXX5beXFdT2M=');
 
 
-ReactDOM.render(<Main/>, document.getElementById('app'));
+ReactDOM.render(<Main />, document.getElementById('app'));
 
 if (module.hot) { // enables hot module replacement if plugin is installed
- module.hot.accept();
+    module.hot.accept();
 }
