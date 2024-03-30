079db8ced0aa64cfbb0f8551a92905216eddb6cf
wantwi <wantwi28@gmail.com>
2024-03-30 23:15:45 +0000

    change base url to test

--- src/api/CustomAxios.js
+++ src/api/CustomAxios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:9050/api";
+// export const BASE_URL = "http://localhost:9050/api";
 // export const SOCKET_BASE_URL = 'https://backendapi-v2.akwaabaevolution.com'
-// export const BASE_URL = "https://api.tinatettherbalpos.com/api";
+export const BASE_URL = "https://api.tinatettherbalpos.com/api";
 // export const BASE_URL = "https://backendapi.akwaabaevolution.com/api"
 
 export const CustomAxios = axios.create({
